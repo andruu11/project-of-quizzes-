c36f68446b05f5021dd8634afc60bce6fc18bd69
Chiplex <raaz11chip@gmail.com>
2018-12-07 05:51:12 -0400

    Titulo de inicio arreglado

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller 
 {
 	public function index()
-	{				
+	{
+		$data['title'] = 'Inicio';
 		$data['_acceso'] = $this->session->userdata('acceso') ? $this->session->userdata('acceso') : FALSE;		
 		
 		$this->load->view('side/header', $data);
